8a16268794a5a0eed6aa519f10ffc542a248db55
Rony Vidaur <rony.vidaur@gmail.com>
2017-03-09 13:50:41 -0600

    proyecto terminado

--- proyectos/01 reloj/main.js
+++ proyectos/01 reloj/main.js
@@ -4,6 +4,8 @@
 // document.getElementsByTagName('tagName')
 // document.querySelector('query')
 // document.querySelectorAll('query')
+//style.transform
+// setInterval
 var agujaHoras = document.querySelector('.aguja-horas')
 // console.log(agujaHoras)
 var agujaMinutos = document.querySelector('.aguja-minutos')
@@ -16,6 +18,18 @@ function obtenerHora() {
   // getMinutes()
   // getHours()
   var hora = fecha.getHours()
+  var minutos = fecha.getMinutes()
+  var segundos = fecha.getSeconds()
+  var gradosHora = ((hora/12) * 360) + 90
+  var gradosMinutos = ((minutos/60)*360) + 90
+  var gradosSegundos = ((segundos/60)*360) + 90
+
+  agujaHoras.style.transform = "rotate("+ gradosHora + "deg)"
+  agujaMinutos.style.transform = "rotate("+ gradosMinutos+ "deg)"
+  agujaSegundos.style.transform = "rotate("+ gradosSegundos + "deg)"
+
   console.log(hora)
+  console.log(minutos)
+  console.log(segundos)
 }
-obtenerHora()
+setInterval(obtenerHora, 1000)
