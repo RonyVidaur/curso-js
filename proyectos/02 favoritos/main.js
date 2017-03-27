8c51b81e17a811bc47f7941f2b67ee70a48aea99
Rony Vidaur <rony.vidaur@gmail.com>
2017-03-27 13:37:30 -0600

    dia 3

--- proyectos/02 favoritos/main.js
+++ proyectos/02 favoritos/main.js
@@ -3,17 +3,28 @@
 // preventDefault
 var formularioPrincipal = document.getElementById('formularioPrincipal').addEventListener('submit', guardarFavorito)
 
+
 function guardarFavorito(e) {
   // Evitar que se ejecute el comportamiento por defecto
-  e.preventDefault()
   var nombre = document.getElementById('nombre').value
   var url = document.getElementById('url').value
 
-  var favoritos = {
+  var favorito = {
     nombre: nombre,
     url: url
   }
-  localStorage.setItem("apellido", "Perez")
-  localStorage.setItem('test', 'Hello World')
+
+  // Stringify
+  // localStorage.setItem("apellido", "Perez")
+  // localStorage.setItem('test', 'Hello World')
+  if(localStorage.getItem('favoritos') === null) {
+    var favoritos = []
+    favoritos.push(favorito)
+    localStorage.setItem('favoritos', JSON.stringify(favorito))
+  } else {
+    var favoritos = JSON.parse(localStorage.getItem('favoritos'))
+    favoritos.push(favorito)
+  }
+  e.preventDefault()
   console.log(favoritos)
 }
