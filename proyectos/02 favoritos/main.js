62b83aee61d08afe27e9bb66d5e1802139d8ffff
Rony Vidaur <rony.vidaur@gmail.com>
2017-03-31 13:14:43 -0600

    corregido objeto favoritos

--- proyectos/02 favoritos/main.js
+++ proyectos/02 favoritos/main.js
@@ -1,30 +1,22 @@
-// Nuevos conceptos
-// addEventListener
-// preventDefault
-var formularioPrincipal = document.getElementById('formularioPrincipal').addEventListener('submit', guardarFavorito)
+document.getElementById('formularioPrincipal').addEventListener('submit', guardarFavorito)
 
-
-function guardarFavorito(e) {
-  // Evitar que se ejecute el comportamiento por defecto
-  var nombre = document.getElementById('nombre').value
-  var url = document.getElementById('url').value
+function  guardarFavorito(e) {
+  var nombreSitio = document.getElementById('nombreSitio').value
+  var urlSitio = document.getElementById('urlSitio').value
 
   var favorito = {
-    nombre: nombre,
-    url: url
+    nombre: nombreSitio,
+    url: urlSitio
   }
 
-  // Stringify
-  // localStorage.setItem("apellido", "Perez")
-  // localStorage.setItem('test', 'Hello World')
-  if(localStorage.getItem('favoritos') === null) {
+  if (localStorage.getItem('favoritos') === null) {
     var favoritos = []
     favoritos.push(favorito)
-    localStorage.setItem('favoritos', JSON.stringify(favorito))
+    localStorage.setItem('favoritos', JSON.stringify(favoritos))
   } else {
     var favoritos = JSON.parse(localStorage.getItem('favoritos'))
     favoritos.push(favorito)
+    localStorage.setItem('favoritos', JSON.stringify(favoritos))
   }
   e.preventDefault()
-  console.log(favoritos)
 }
