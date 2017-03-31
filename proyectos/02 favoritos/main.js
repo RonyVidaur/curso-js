505ff18d596058446343da22a6bc9ee1669241f5
Rony Vidaur <rony.vidaur@gmail.com>
2017-03-31 13:42:21 -0600

    cargar favoritos

--- proyectos/02 favoritos/main.js
+++ proyectos/02 favoritos/main.js
@@ -8,15 +8,33 @@ function  guardarFavorito(e) {
     nombre: nombreSitio,
     url: urlSitio
   }
-
+  // si no existe el objeto favoritos en localStorage
   if (localStorage.getItem('favoritos') === null) {
     var favoritos = []
     favoritos.push(favorito)
     localStorage.setItem('favoritos', JSON.stringify(favoritos))
+    // si ya existe el objeto en localStorage
   } else {
     var favoritos = JSON.parse(localStorage.getItem('favoritos'))
     favoritos.push(favorito)
     localStorage.setItem('favoritos', JSON.stringify(favoritos))
   }
+  // evitar que la pagina se recargue al presionar el boton submit
   e.preventDefault()
+}
+
+function cargarFavoritos() {
+  var resultadoFavoritos = document.getElementById('resultadoFavoritos')
+  var favoritos = JSON.parse(localStorage.getItem('favoritos'))
+  for (var i = 0; i < favoritos.length; i++) {
+    var nombre = favoritos[i].nombre
+    var url = favoritos[i].url
+    // innerHTML - mostrar resultado en pantalla
+    resultadoFavoritos.innerHTML += '<div class="well"' +
+                                      "<h3>"+ nombre +
+                                      '<a target="_blank" class="btn btn-default" href="' + url + '">Visitar</a>' +
+                                      '<a class="btn btn-danger" href="#"'+ '">Borrar</a>' +  
+                                      '</h3>' +
+                                    '</div>'
+  }
 }
